bfa9babb35448b4012e014050cd094c1252d8835
Victor Westerlund <victor.vesterlund@gmail.com>
2021-06-03 13:47:15 +0200

    Added theme color change listener

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -4,3 +4,16 @@ if(navigator.serviceWorker) {
 		scope: "/"
 	});
 }
+
+const theme = window.matchMedia("(prefers-color-scheme: dark)");
+
+// Set theme color
+function updateTheme() {
+	// Get theme color from stylesheet
+	const color = window.getComputedStyle(document.body).getPropertyValue("--color-background");
+	document.querySelector("meta[name='theme-color']").setAttribute("content",color);
+}
+
+// Set theme color and listen for changes
+theme.addEventListener("change",updateTheme);
+updateTheme(theme);
