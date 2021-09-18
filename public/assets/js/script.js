3005b28d8cb245685bf4a1dc7fb076b164052905
Victor Westerlund <victor.vesterlund@gmail.com>
2021-09-18 13:45:48 +0200

    v9.0-pre-c
    Finalized default interaction "copyText()"

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -32,9 +32,11 @@ const interactions = {
 		// Remove transition CSS when finished. Wonky resize effects otherwise
 		setTimeout(() => menu.style.removeProperty("transition"),transition + 1);
 	},
+	// Open page defined with data-value as a card
 	newCard: (event) => {
 		const module = import("./modules/Modals.mjs");
 		const interactions = {
+			// Like newCard() except it closes the previous card
 			getContact: (event) => {
 				const service = event.target.dataset.value;
 				module.then(modals => {
@@ -43,19 +45,25 @@ const interactions = {
 					card.openPage(service);
 				});
 			},
+			// Copy text defined in data-value to clipboard and play animation
 			copyText: (event) => {
-				event.target.classList.add("copied");
-				const copied = document.createElement("p");
-				copied.innerText = "copied!";
-				event.target.appendChild(copied);
-				
-				setTimeout(() => {
-					event.target.classList.remove("copied");
-					destroy(copied);
-				},1000);
+				const copy = navigator.clipboard.writeText(event.target.dataset.value);
+				copy.then(() => {
+					event.target.classList.add("copied");
+					const copied = document.createElement("p");
+					copied.innerText = "copied!";
+					event.target.appendChild(copied);
+					
+					// Reset button state
+					setTimeout(() => {
+						event.target.classList.remove("copied");
+						destroy(copied);
+					},1000);
+				});
 			}
 		};
 
+		// Create card and open the specified page asynchronously
 		module.then(modals => {
 			const card = new modals.Card(interactions);
 			card.openPage(event.target.dataset.value);
@@ -67,5 +75,5 @@ const interactions = {
 const theme = window.matchMedia("(prefers-color-scheme: dark)");
 theme.addEventListener("change",updateTheme);
 
-new Interaction(interactions,document.body);
+new Interaction(interactions,document.body); // Initialize default interactions
 updateTheme();
