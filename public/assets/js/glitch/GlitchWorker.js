173007ba716228ff94f203eb8457c788e3df5296
Victor Westerlund <victor@victorwesterlund.com>
2022-02-18 15:37:14 +0100

    wip(22w7f): refactor glitch

---
+++ public/assets/js/glitch/GlitchWorker.js
@@ -0,0 +1,48 @@
+importScripts("./Generator.mjs");
+
+class GlitchWorker extends Generator {
+	constructor() {
+		super();
+
+		// Delay between these values
+		this.config = {
+			glitch: { min: 500, max: 2500 },
+			randBg: { min: 5000, max: 5000 }
+		}
+
+		this._timers = {};
+
+		self.addEventListener("message", event => this.message(event));
+		self.postMessage("READY");
+	}
+
+	test() {
+		console.log("yes");
+	}
+
+	// Run a scoped function on a random interval between
+	queue(func) {
+		clearTimeout(this._timers[func]);
+		const next = Generator.randInt(this.config[func].min, this.config[func].max);
+		this._timers[func] = setTimeout(() => this.queue(func), next);
+
+		this[func]?.();
+	}
+
+	// Event handler for messages from parent thread
+	message(event) {
+		const data = typeof event.data === "object" ? event.data : [event.data];
+
+		switch(data[0]) {
+			case "START":
+				this.bg.dir = data[1];
+				this.randBg();
+				for(const func of Object.keys(this.config)) {
+					this.queue(func);
+				}
+				break;
+		}
+	}
+}
+
+self.glitch = new GlitchWorker();
--- public/assets/js/script.mjs
+++ public/assets/js/script.mjs
@@ -1,29 +1,14 @@
-import { default as Glitch } from "./modules/Glitch.mjs";
+import { default as Glitch } from "./glitch/Glitch.mjs";
 
 const logging = "https://victorwesterlund-logging-dnzfgzf6za-lz.a.run.app";
 
-async function openModal(name) {
-    const module = await import("./modules/Modal.mjs");
-    if(!module) {
-        alert("Failed to import module.");
-        return;
-    }
-
-    const modal = new module.default();
-    modal.assetsRoot = window.location.href;
-}
-
-// Bind click listerners to all links
+// Log link clicks
 for(let link of document.getElementsByTagName("a")) {
     link.addEventListener("click", event => {
         event.preventDefault();
-        navigator?.sendBeacon(logging, event); // Log link click
-
-        // Treat tag without func data attribute as a normal link
-        if(!"func" in event.target.dataset) window.location.href = event.target.href;
-
-        openModal(event.target.getAttribute("href"));
+        navigator?.sendBeacon(logging, event);
+        window.location.href = event.target.href;
     });
 }
 
-new Glitch(document.body);
+window.glitch = new Glitch(document.body);
